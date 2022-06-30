d10bcc372bcea556bfd9444f2ac1b0c5a3300884
Nozeil <nozeilwrk@gmail.com>
2022-06-30 10:09:13 +0300

    fix: add apiKey

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '4c060b36cda74bf08f8f56cac47ab4c2', // получите свой ключ https://newsapi.org/
         });
     }
 }
