6cc5a4f3ffe8afc67bce2aaefeeafab485a731b4
Nozeil <nozeilwrk@gmail.com>
2022-07-02 12:51:05 +0300

    refactor: implement code typing in app.ts

--- migration-to-ts/src/components/app/app.ts
+++ migration-to-ts/src/components/app/app.ts
@@ -2,15 +2,20 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
+    controller: AppController;
+    view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+    start(): void {
+        (document.querySelector('.sources') as HTMLDivElement).addEventListener('click', (e: MouseEvent): void =>
+            this.controller.getNews(e, (data) => {
+                this.view.drawNews(data);
+            })
+        );
         this.controller.getSources((data) => this.view.drawSources(data));
     }
 }
