ebaa84fad1854375223dc7226e70d16fd7de9f82
Nozeil <nozeilwrk@gmail.com>
2022-07-02 12:00:02 +0300

    refactor: implement code typing in controller.ts

--- migration-to-ts/src/components/controller/controller.ts
+++ migration-to-ts/src/components/controller/controller.ts
@@ -1,7 +1,8 @@
+import { Callback } from '../interfaces/interfaces';
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: Callback): void {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,13 +11,13 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: MouseEvent, callback: Callback): void {
+        let target: HTMLDivElement = e.target as HTMLDivElement;
+        const newsContainer: HTMLDivElement = e.currentTarget as HTMLDivElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId: string = target.getAttribute('data-source-id') as string;
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +32,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLDivElement;
         }
     }
 }
