852f796fc324e0abb58453d9cf075a73b953c5c5
Nozeil <nozeilwrk@gmail.com>
2022-07-03 19:03:39 +0300

    fix: change domen name

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '4c060b36cda74bf08f8f56cac47ab4c2', // получите свой ключ https://newsapi.org/
         });
     }
